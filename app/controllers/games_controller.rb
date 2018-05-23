d2a715df1ba86c185446ea7504bae0951a5fa946
Antoine Brissy <antoinebrissy@gmail.com>
2018-05-23 18:43:38 +0200

    create a bet when you invitate a friend

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,22 +17,41 @@ def create
    @bet.challenger = true
    @bet.game = @game
   if @game.save && @bet.save
-   redirect_to game_path(@game)
+   redirect_to invite_path(@game)
   else
    render :new
   end
+end
+
+
+def invite; end
+
+
+def save_invite
+  # raise
+ @game = Game.find(params[:id])
+ email = params[:user][:email]
+ user = User.invite!(email: email)
+ bet = Bet.new(user: user, game: @game, status: "pending", challenger: false)
+ if bet.save
+  redirect_to game_path(@game)
+ else
+  render :invite
+ end
 end
 
 def show
   @game = Game.find(params[:id])
 end
 
-
-
 private
 
 def game_params
  params.require(:game).permit(:title, :description, :price, :dead_line, :photo)
+end
+
+def user_params
+ params.require(:user).permit(:email)
 end
 
 
--- app/models/bet.rb
+++ app/models/bet.rb
@@ -23,5 +23,5 @@
 
 class Bet < ApplicationRecord
   belongs_to :game
-  belongs_to :user
+  belongs_to :user, optional: true
 end
