77c1b658f18a01b19658727388e35d17c818b25f
Jose Veiga <veiga.jose@free.fr>
2018-05-23 10:55:21 +0200

    test mobile first et activation des liens games

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: bets
+#
+#  id         :bigint(8)        not null, primary key
+#  challenger :boolean
+#  status     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  game_id    :bigint(8)
+#  user_id    :bigint(8)
+#
+# Indexes
+#
+#  index_bets_on_game_id  (game_id)
+#  index_bets_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (game_id => games.id)
+#  fk_rails_...  (user_id => users.id)
+#
+
 class Bet < ApplicationRecord
   belongs_to :game
   belongs_to :user
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: games
+#
+#  id          :bigint(8)        not null, primary key
+#  dead_line   :datetime
+#  description :text
+#  photo       :string
+#  price       :integer
+#  status      :string
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Game < ApplicationRecord
   has_many :prizes
   has_many :bets
